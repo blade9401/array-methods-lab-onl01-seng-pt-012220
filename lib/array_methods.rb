1f9ce1c4d41e3b3f1160a58ddc2d74ba6d11bb3a
Eric murray <ericwmurray1994@gmail.com>
2020-01-28 05:49:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-def using_include(array, element)
+def using_include(array, element1, element2)
   using_include = ["wow","I","am","really","learning","arrays!"]
   using_include.include?("wow","tardis")
 end
